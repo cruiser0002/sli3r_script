6d2c362d9e8aab8eee27d7b1d34b07fb1b981020
cruiser0002 <cruiser0002@gmail.com>
2016-03-30 23:18:21 -0700

    initial

---
+++ resin_retracts.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+#print ('Number of arguments:', len(sys.argv), 'arguments.')
+#print ('Argument List:', str(sys.argv))
+
+if len(sys.argv) != 2:
+    print ('usage: python scriptname.py filename')
+    exit(1)
+
+file_location = sys.argv[1]
+#file_location = "test.gcode"
+#output_file = "out.gcode"
+
+#with open(file_location, 'r+') as f:
+    #data = f.read()
+    #print ('file length:', len(data))
+
+layer_data = []
+layer_distance = []
+layer_num = 0
+
+
+def isLayerChange(line):
+    match = re.match(r'^[g][01].+[z].+', line, re.I)
+    if match:
+        return 1
+    return 0
+
+
+one_layer = []
+def process(line):
+    global layer_num
+    global one_layer
+    global layer_data
+    #append line into the end of this layer's data
+
+    if isLayerChange(line):
+        layer_num += 1
+        layer_data += [one_layer]
+        one_layer = []
+    one_layer += [line]
+
+with open(file_location, 'r') as f:
+    #data = f.read()
+    #print ('file length:', len(data))
+    for line in f:
+        process(line)
+
+if len(layer_data) < 2:
+    exit(1)
+
+first_layer = layer_data[1]
+feed_rate = 1
+first_layer_sum_extrusion = 0
+first_layer_threshold = 20 #config
+first_layer_repeat = 3 #config
+
+
+previous_extrusion_distance = 0
+current_extrusion_distance = 0
+previous_sum_distance = 0
+
+
+
+new_data = []
+new_layer = []
+extra_lift_code = ['G91 ; relative position\n', 'G1 Z5\n', 'G1 Z-5\n', 'G90 ; absolute position\n']
+
+new_data += [layer_data[0]]
+
+
+for line in first_layer:
+    new_layer += [line]
+    match = re.match(r'^[g][01].+[f]([0-9]+[\.]?[0-9]*)', line, re.I)
+    if match:
+        feed_rate = float(match.group(1))
+    match = re.match(r'^[g][01].+[e]([0-9]+[\.]?[0-9]*)', line, re.I)
+    if match:
+        current_extrusion_distance = float(match.group(1))
+        delta = current_extrusion_distance - previous_extrusion_distance
+        if delta >= 0:
+            first_layer_sum_extrusion += delta / feed_rate
+        else:
+            first_layer_sum_extrusion += current_extrusion_distance / feed_rate
+
+        previous_extrusion_distance = current_extrusion_distance
+    if (current_extrusion_distance - previous_sum_distance) >= first_layer_threshold:
+        #print("first layer insertion needed")
+        new_layer += extra_lift_code
+        previous_sum_distance = current_extrusion_distance
+
+new_data += [new_layer] * first_layer_repeat
+#print(new_data)
+#print(first_layer_sum_extrusion)
+
+
+
+
+extra_lift_retraction_factor = 3.0 #config
+extra_lift_threshold = first_layer_sum_extrusion / extra_lift_retraction_factor
+
+feed_rate = 1
+
+for layer in layer_data[1:]:
+    previous_extrusion_distance = 0
+    current_extrusion_distance = 0
+    sum_extrusion = 0
+    previous_sum_distance = 0
+    new_layer = []
+
+    for line in layer:
+        match = re.match(r'^[g][01].+[e]([0-9]+[\.]?[0-9]*)', line, re.I)
+        if match:
+            previous_extrusion_distance = float(match.group(1))
+            #print(previous_extrusion_distance)
+            break
+
+    #print("first:" + str(previous_extrusion_distance))
+
+    for line in layer:
+        new_layer += [line]
+        #find out if the feed rate changed
+        match = re.match(r'^[g][01].+[f]([0-9]+[\.]?[0-9]*)', line, re.I)
+        if match:
+            feed_rate = float(match.group(1))
+        #find out if the line contains extrusion
+        match = re.match(r'^[g][01].+[e]([0-9]+[\.]?[0-9]*)', line, re.I)
+        if match:
+            current_extrusion_distance = float(match.group(1))
+            delta = current_extrusion_distance - previous_extrusion_distance
+
+            #print("prev:" + str(previous_extrusion_distance))
+            #print("delta:" + str(delta))
+            #print("current:" + str(current_extrusion_distance))
+
+            previous_extrusion_distance = current_extrusion_distance
+            if delta >= 0:
+                sum_extrusion += delta / feed_rate
+            else:
+                sum_extrusion += current_extrusion_distance / feed_rate
+
+            if (sum_extrusion - previous_sum_distance) >= extra_lift_threshold:
+                #print("insertion needed")
+                new_layer += extra_lift_code
+                previous_sum_distance = sum_extrusion
+
+    #print(sum_extrusion)
+    #print("Extra lift for Z operation")
+    new_layer += extra_lift_code
+    new_data += [new_layer]
+
+with open(file_location, 'w') as f:
+    for layer in new_data:
+        for line in layer:
+            f.write(line)
